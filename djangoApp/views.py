7e52650f6f4ad8e1914832cc7b47eced3a1bbcb7
Sai Manasa <vedantamsaimanasa@gmail.com>
2022-04-12 20:25:12 -0500

    Upto-task-5b

--- djangoApp/views.py
+++ djangoApp/views.py
@@ -9,8 +9,6 @@ from djangoApp.models import *
 class IndexView(ListView):
     template_name = "index.html"
     def get_queryset(self):
-        result=Diseases_Symptoms.objects.all()
+        result = Diseases_Symptoms.objects.all()
+        result = result.distinct().order_by()
         return result
-
-
-        
