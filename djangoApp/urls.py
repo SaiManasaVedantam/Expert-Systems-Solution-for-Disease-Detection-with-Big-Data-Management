4ddfe1a321d94002cc92492513cb7b48305a0530
Tanvi Patil <tanvipatil@Tanvis-MBP.lan>
2022-05-08 10:50:41 -0500

    graphs added

--- djangoApp/urls.py
+++ djangoApp/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('', views.get_queryset, name='home'),
     path('result/', views.result, name='result'),
+    path('api', views.ChartData.as_view()),
 ]
